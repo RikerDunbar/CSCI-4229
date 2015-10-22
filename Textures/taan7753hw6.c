e02a95fe52df03b49c1ef37a9cda7e50da9a5ace
Taylor Andrews <andrewstaylorj@gmail.com>
2015-10-21 22:48:42 -0600

    Textures Assignment

---
+++ Textures/taan7753hw6.c
@@ -0,0 +1,522 @@
+/*
+ *  HW6
+ *  Modified from my HW5
+ *
+ *  Key bindings:
+ *  w/s        Move in first person
+ *  a/d        Rotate camera in first person
+ *  space      Pause light rotation
+ *  </>        Manually move light left/right
+ *  m          Toggle between perspective and orthogonal
+ *  f          Toggle first person veiw on and off (higher priority than the other two view modes)
+ *  1/2        Changes field of view for perspective
+ *  9          Toggle axes
+ *  arrows     Change view angle
+ *  PgDn/PgUp  Zoom
+ *  0          Reset view angle
+ *  ESC        Exit
+ */
+ 
+#include "CSCIx229.h"
+
+#define PI 3.1416
+
+int axes=0;       //  Display axes
+int mode=0;       //  Projection mode
+int fp=0;
+int th=0;         //  Azimuth of view angle
+int ph=0;         //  Elevation of view angle
+int fov=55;       //  Field of view (for perspective)
+double asp=1;     //  Aspect ratio
+double dim=5.0;   //  Size of world
+
+int rot = 0.0; //rotation var for first person
+
+/* Eye coords */
+double Ex = 0;
+double Ey = 0;
+double Ez = 5;
+
+/* Camera looking coords 
+ * Don't need a y coord because up-down view cannot be altered in fp 
+ */
+double Cx = 0; 
+double Cz = 0; 
+
+int move = 1;
+
+// Light values
+int one       =   1;  // Unit value
+int distance  =   5;  // Light distance
+int inc       =  10;  // Ball increment
+int smooth    =   1;  // Smooth/Flat shading
+int local     =   0;  // Local Viewer Model
+int emission  =   0;  // Emission intensity (%)
+int ambient   =  30;  // Ambient intensity (%)
+int diffuse   = 100;  // Diffuse intensity (%)
+int specular  =   0;  // Specular intensity (%)
+int shininess =   0;  // Shininess (power of two)
+float shinyvec[1];    // Shininess (value)
+int zh        =  90;  // Light azimuth
+float ylight  =   0;  // Elevation of light
+
+unsigned int texture[3]; // Texture names
+
+typedef struct {
+   double x;
+   double y;
+   double z;
+} points_t;
+
+/*
+ *  Draw vertex in polar coordinates with normal
+ *  Adapted from ex13
+ */
+static void Vertex(double th,double ph)
+{
+   double x = Sin(th)*Cos(ph);
+   double y = Cos(th)*Cos(ph);
+   double z =         Sin(ph);
+   //  For a sphere at the origin, the position
+   //  and normal vectors are the same
+   glNormal3d(x,y,z);
+   glVertex3d(x,y,z);
+}
+
+/*
+ *  Draw a ball
+ *     at (x,y,z)
+ *     radius (r)
+ *  Adapted from ex13
+ */
+static void ball(double x,double y,double z,double r)
+{
+   int th,ph;
+   float yellow[] = {1.0,1.0,0.0,1.0};
+   float Emission[]  = {0.0,0.0,0.01*emission,1.0};
+   //  Save transformation
+   glPushMatrix();
+   //  Offset, scale and rotate
+   glTranslated(x,y,z);
+   glScaled(r,r,r);
+   //  White ball
+   glColor3f(1,1,1);
+   glMaterialfv(GL_FRONT,GL_SHININESS,shinyvec);
+   glMaterialfv(GL_FRONT,GL_SPECULAR,yellow);
+   glMaterialfv(GL_FRONT,GL_EMISSION,Emission);
+   //  Bands of latitude
+   for (ph=-90;ph<90;ph+=inc)
+   {
+      glBegin(GL_QUAD_STRIP);
+      for (th=0;th<=360;th+=2*inc)
+      {
+         Vertex(th,ph);
+         Vertex(th,ph+inc);
+      }
+      glEnd();
+   }
+   //  Undo transofrmations
+   glPopMatrix();
+}
+
+/*
+ *  Set projection
+ */
+static void myProject()
+{
+   //  Tell OpenGL we want to manipulate the projection matrix
+   glMatrixMode(GL_PROJECTION);
+   //  Undo previous transformations
+   glLoadIdentity();
+   //  Perspective transformation
+   if(fp) {
+      gluPerspective(fov,asp,dim/4,4*dim);
+   }
+   else {
+      if (mode)
+         gluPerspective(fov,asp,dim/4,4*dim);
+      //  Orthogonal projection
+      else
+         glOrtho(-asp*dim,+asp*dim, -dim,+dim, -dim,+dim);
+   }  
+   //  Switch to manipulating the model matrix
+   glMatrixMode(GL_MODELVIEW);
+   //  Undo previous transformations
+   glLoadIdentity();
+}
+
+/*
+ *  Adapted from Example 7 given on Moodle
+ *  Draw a cylinder
+ *     at (x,y,z)
+ *     dimentions (dx,dy,dz)
+ *     rotated th about the z axis
+ *     given top & bottom color
+ *     given side color
+ */
+void drawCylinder(double x,double y,double z,
+                 double dx,double dy,double dz,
+                 double th, char lcol, char scol) {
+   double radius = 0.3, height = 0.2;
+
+   //  Set specular color to white
+   float white[] = {1,1,1,1};
+   float black[] = {0,0,0,1};
+   glMaterialfv(GL_FRONT_AND_BACK,GL_SHININESS,shinyvec);
+   glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,white);
+   glMaterialfv(GL_FRONT_AND_BACK,GL_EMISSION,black);
+
+   glPushMatrix();
+
+   //  Transform cylinder
+   glTranslated(x,y,z);
+   glRotated(th,0,0,1);
+   glScaled(dx,dy,dz);
+
+   //  Enable textures
+   glEnable(GL_TEXTURE_2D);
+   glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_MODULATE);
+
+
+
+   //Set colors
+   if (scol == 'r'){
+      glColor3f(1.0, 0.0, 0.0); //Set color to red
+   } else if (scol == 'g') {
+      glColor3f(0.0, 1.0, 0.0); //Set color to green
+   } else if (scol == 'b') {
+      glColor3f(0.0, 0.0, 1.0); //Set color to blue
+   } else if (scol == 'w') {
+      glColor3f(1.0, 1.0, 1.0); //Set color to white
+   } else {
+      glColor3f(0.8, 0.8, 0.8); //Default color to gray
+   }
+
+   glBindTexture(GL_TEXTURE_2D,texture[0]);
+
+   double j;
+   glBegin(GL_QUAD_STRIP);
+   for (j = 0; j <= 360; j+=.125) {
+      const float tc = (j / (float) 360);
+
+      double x = radius * Cos(j);
+      double y = height;
+      double z = radius * Sin(j);
+
+      glNormal3d(Cos(j), 0, Sin(j));
+
+      glTexCoord2f(-tc, 0.0); glVertex3d(x, -y, z);
+      glTexCoord2f(-tc, 1.0); glVertex3d(x, y, z);
+   }
+   glEnd();
+
+   //Set colors
+   if (lcol == 'r'){
+      glColor3f(1.0, 0.0, 0.0); //Set color to red
+   } else if (lcol == 'g') {
+      glColor3f(0.0, 1.0, 0.0); //Set color to green
+   } else if (lcol == 'b') {
+      glColor3f(0.0, 0.0, 1.0); //Set color to blue
+   } else if (lcol == 'w') {
+      glColor3f(1.0, 1.0, 1.0); //Set color to white
+   } else {
+      glColor3f(0.8, 0.8, 0.8); //Default color to gray
+   }
+
+   double i; 
+
+   glBindTexture(GL_TEXTURE_2D,texture[1]);
+
+   glNormal3d(0,1,0);
+
+   /* Top of Cylinder */
+   glBegin(GL_TRIANGLE_FAN);
+      glTexCoord2f(0.5,0.5); 
+      glVertex3d(0.0, height, 0.0);
+
+      for(i = 0.0; i <= 360; i+=10) {
+         glTexCoord2f(-0.5*Cos(i)+0.5, 0.5*Sin(i)+0.5);
+         glVertex3d(radius * Cos(i), height, radius * Sin(i));
+      }
+   glEnd();
+
+   glBindTexture(GL_TEXTURE_2D,texture[2]);
+   glNormal3d(0,-1,0);
+
+   /* Bottom of Cylinder */
+   glBegin(GL_TRIANGLE_FAN);
+      glTexCoord2f(0.5,0.5); 
+      glVertex3d(0.0, -height, 0.0);
+
+      for(i = 0.0; i <= 360; i+=10) {
+         glTexCoord2f(0.5*Cos(i)+0.5, 0.5*Sin(i)+0.5);
+         glVertex3d(radius * Cos(i), -height, radius * Sin(i));
+      }
+   glEnd();
+
+   glPopMatrix();
+   glDisable(GL_TEXTURE_2D); 
+}
+
+/*
+ *  Display the scene
+ *  Taken from HW3 and Example 9
+ */
+void display()
+{
+   const double len=1.5;  //  Length of axes
+   //  Erase the window and the depth buffer
+   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+   //  Enable Z-buffering in OpenGL
+   glEnable(GL_DEPTH_TEST);
+   //  Undo previous transformations
+   glLoadIdentity();
+
+   if (fp) {
+      Cx = +2*dim*Sin(rot); //Ajust the camera vector based on rot
+      Cz = -2*dim*Cos(rot);
+
+      gluLookAt(Ex,Ey,Ez, Cx+Ex,Ey,Cz+Ez, 0,1,0); //  Use gluLookAt, y is the up-axis
+
+   }
+   else {
+      //  Perspective - set eye position
+      if (mode)
+      {
+         double px = -2*dim*Sin(th)*Cos(ph);
+         double py = +2*dim        *Sin(ph);
+         double pz = +2*dim*Cos(th)*Cos(ph);
+         gluLookAt(px,py,pz , 0,0,0 , 0,Cos(ph),0);
+      }
+      //  Orthogonal - set world orientation
+      else
+      {
+         glRotatef(ph,1,0,0);
+         glRotatef(th,0,1,0);
+      }
+   }
+
+   glShadeModel(GL_SMOOTH);
+   //  Translate intensity to color vectors
+   float Ambient[]   = {0.01*ambient ,0.01*ambient ,0.01*ambient ,1.0};
+   float Diffuse[]   = {0.01*diffuse ,0.01*diffuse ,0.01*diffuse ,1.0};
+   float Specular[]  = {0.01*specular,0.01*specular,0.01*specular,1.0};
+   //  Light position
+   float Position[]  = {distance*Cos(zh),ylight,distance*Sin(zh),1.0};
+   //  Draw light position as ball (still no lighting here)
+   glColor3f(1,1,1);
+   ball(Position[0],Position[1],Position[2] , 0.1);
+   //  OpenGL should normalize normal vectors
+   glEnable(GL_NORMALIZE);
+   //  Enable lighting
+   glEnable(GL_LIGHTING);
+   //  Location of viewer for specular calculations
+   glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER,local);
+   //  glColor sets ambient and diffuse color materials
+   glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
+   glEnable(GL_COLOR_MATERIAL);
+   //  Enable light 0
+   glEnable(GL_LIGHT0);
+   //  Set ambient, diffuse, specular components and position of light 0
+   glLightfv(GL_LIGHT0,GL_AMBIENT ,Ambient);
+   glLightfv(GL_LIGHT0,GL_DIFFUSE ,Diffuse);
+   glLightfv(GL_LIGHT0,GL_SPECULAR,Specular);
+   glLightfv(GL_LIGHT0,GL_POSITION,Position);
+
+   /* Draw 4 Clyinders */   
+   drawCylinder(0, 0, 0, 1, 1, 1, 0, 'b', 'b');
+   drawCylinder(.5, .5, .5, 1, 3, 1, 20, 'q', 'q');
+   drawCylinder(-.9, 0, 0, 2, 2, 1, 90, 'g', 'g');
+   drawCylinder(-.6, -.6, -.8, .3, .5, .3, -45, 'r', 'r');
+
+//  Draw axes - no lighting from here on
+   glDisable(GL_LIGHTING);
+   glColor3f(1,1,1);
+   if (axes)
+   {
+      glBegin(GL_LINES);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(len,0.0,0.0);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(0.0,len,0.0);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(0.0,0.0,len);
+      glEnd();
+      //  Label axes
+      glRasterPos3d(len,0.0,0.0);
+      Print("X");
+      glRasterPos3d(0.0,len,0.0);
+      Print("Y");
+      glRasterPos3d(0.0,0.0,len);
+      Print("Z");
+   }
+
+   //  Display parameters
+   glWindowPos2i(5,5);   
+   if (fp) {
+      Print("FP=On View Angle=%d",rot);
+   }
+   else {
+      Print("FP=Off Angle=%d,%d  Dim=%.1f FOV=%d Projection=%s",th,ph,dim,fov,mode?"Perpective":"Orthogonal");
+   }
+
+   //  Render the scene and make it visible
+   ErrCheck("display");
+   glFlush();
+   glutSwapBuffers();
+}
+
+/*
+ *  GLUT calls this routine when the window is resized
+ *  Adopted from ex13
+ */
+void idle()
+{
+   //  Elapsed time in seconds
+   double t = glutGet(GLUT_ELAPSED_TIME)/1000.0;
+   zh = fmod(90*t,360.0);
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when an arrow key is pressed
+ */
+void special(int key,int x,int y)
+{
+   if(!fp) {
+      //  Right arrow key - increase angle by 5 degrees
+      if (key == GLUT_KEY_RIGHT)
+         th += 5;
+      //  Left arrow key - decrease angle by 5 degrees
+      else if (key == GLUT_KEY_LEFT)
+         th -= 5;
+      //  Up arrow key - increase elevation by 5 degrees
+      else if (key == GLUT_KEY_UP)
+         ph += 5;
+      //  Down arrow key - decrease elevation by 5 degrees
+      else if (key == GLUT_KEY_DOWN)
+         ph -= 5;
+      //  PageUp key - increase dim
+      else if (key == GLUT_KEY_PAGE_UP && dim>1)
+         dim -= 0.1;
+      //  PageDown key - decrease dim
+      else if (key == GLUT_KEY_PAGE_DOWN)
+         dim += 0.1;
+      //  Keep angles to +/-360 degrees
+      th %= 360;
+      ph %= 360;
+   }
+   //  Update projection
+   myProject();
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when a key is pressed
+ */
+void key(unsigned char ch,int x,int y)
+{
+   //  Exit on ESC
+   if (ch == 27)
+      exit(0);
+   //  Toggle axes
+   else if (ch == '9')
+      axes = 1-axes;
+   //  Pause Light
+   else if (ch == ' ') {
+      move = 1-move;
+   }
+   //  Move light
+   else if (ch == '<')
+      zh += 3;
+   else if (ch == '>')
+      zh -= 3;
+   //  Toggle first person
+   else if (ch == 'f' || ch == 'F')
+   {
+      fp = 1-fp;
+   }
+
+   if (fp) {
+      double dt = 0.05;
+      if (ch == 'w' || ch == 'W'){
+         Ex += Cx*dt; //Update the eye vector based on the camera vector
+         Ez += Cz*dt;
+      }
+      else if (ch == 'a' || ch == 'A'){
+         rot -= 3;
+      }
+      else if (ch == 's' || ch == 'S'){
+         Ex -= Cx*dt;
+         Ez -= Cz*dt;
+      }
+      else if (ch == 'd' || ch == 'D'){
+         rot += 3;
+      }
+
+      //  Keep angles to +/-360 degrees
+      rot %= 360;
+   }
+   else {
+      //  Reset view angle
+      if (ch == '0')
+         th = ph = 0;
+      //  Switch display mode
+      else if (ch == 'm' || ch == 'M')
+         mode = 1-mode;
+      //  Change field of view angle
+      else if (ch == '1')
+         fov--;
+      else if (ch == '2')
+         fov++;
+   }
+   
+   //  Reproject
+   myProject();
+   //  Animate if requested
+   glutIdleFunc(move?idle:NULL);
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when the window is resized
+ */
+void reshape(int width,int height)
+{
+   //  Ratio of the width to the height of the window
+   asp = (height>0) ? (double)width/height : 1;
+   //  Set the viewport to the entire window
+   glViewport(0,0, width,height);
+   //  Set projection
+   myProject();
+}
+
+/*
+ *  Start up GLUT and tell it what to do
+ */
+int main(int argc,char* argv[])
+{
+   //  Initialize GLUT
+   glutInit(&argc,argv);
+   //  Request double buffered, true color window with Z buffering at 600x600
+   glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+   glutInitWindowSize(600,600);
+   glutCreateWindow("Taylor Andrews");
+   //  Set callbacks
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutSpecialFunc(special);
+   glutKeyboardFunc(key);
+   glutIdleFunc(idle);
+   //Load Textures
+   texture[0] = LoadTexBMP("coke.bmp");
+   texture[1] = LoadTexBMP("top.bmp");
+   texture[2] = LoadTexBMP("bottom.bmp");
+   //  Pass control to GLUT so it can interact with the user
+   ErrCheck("init");
+   glutMainLoop();
+   return 0;
+}
