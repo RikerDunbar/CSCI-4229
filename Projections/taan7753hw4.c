7b529c6ce10f7389bd3f0c5220af02028a3576e1
Taylor Andrews <andrewstaylorj@gmail.com>
2015-10-21 22:47:10 -0600

    Added assignment with projections

---
+++ Projections/taan7753hw4.c
@@ -0,0 +1,379 @@
+/*
+ *  HW4
+ *  Modified from my HW3 and example 9 on moodle
+ *
+ *  Key bindings:
+ *  w/s        Move in first person
+ *  a/d        Rotate camera in first person
+ *  m          Toggle between perspective and orthogonal
+ *  f          Toggle first person veiw on and off (higher priority than the other two view modes)
+ *  1/2        Changes field of view for perspective
+ *  9          Toggle axes
+ *  arrows     Change view angle
+ *  PgDn/PgUp  Zoom in and out
+ *  0          Reset view angle
+ *  ESC        Exit
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+//  OpenGL with prototypes for glext
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define PI 3.1416
+
+int axes=0;       //  Display axes
+int mode=0;       //  Projection mode
+int fp=0;
+int th=0;         //  Azimuth of view angle
+int ph=0;         //  Elevation of view angle
+int fov=55;       //  Field of view (for perspective)
+double asp=1;     //  Aspect ratio
+double dim=5.0;   //  Size of world
+
+int rot = 0.0; //rotation var for first person
+
+/* Eye coords */
+double Ex = 0;
+double Ey = 0;
+double Ez = 5;
+
+/* Camera looking coords 
+ * Don't need a y coord because up-down view cannot be altered in fp 
+ */
+double Cx = 0; 
+double Cz = 0; 
+
+//  Macro for sin & cos in degrees
+#define Cos(th) cos(3.1415927/180*(th))
+#define Sin(th) sin(3.1415927/180*(th))
+
+/*
+ *  Convenience routine to output raster text
+ *  Use VARARGS to make this more flexible
+ */
+#define LEN 8192  //  Maximum length of text string
+void Print(const char* format , ...)
+{
+   char    buf[LEN];
+   char*   ch=buf;
+   va_list args;
+   //  Turn the parameters into a character string
+   va_start(args,format);
+   vsnprintf(buf,LEN,format,args);
+   va_end(args);
+   //  Display the characters one at a time at the current raster position
+   while (*ch)
+      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
+}
+
+/*
+ *  Set projection
+ */
+static void Project()
+{
+   //  Tell OpenGL we want to manipulate the projection matrix
+   glMatrixMode(GL_PROJECTION);
+   //  Undo previous transformations
+   glLoadIdentity();
+   //  Perspective transformation
+   if(fp) {
+      gluPerspective(fov,asp,dim/4,4*dim);
+   }
+   else {
+      if (mode)
+         gluPerspective(fov,asp,dim/4,4*dim);
+      //  Orthogonal projection
+      else
+         glOrtho(-asp*dim,+asp*dim, -dim,+dim, -dim,+dim);
+   }  
+   //  Switch to manipulating the model matrix
+   glMatrixMode(GL_MODELVIEW);
+   //  Undo previous transformations
+   glLoadIdentity();
+}
+
+/*
+ *  Adapted from Example 7 given on Moodle
+ *  Draw a cylinder
+ *     at (x,y,z)
+ *     dimentions (dx,dy,dz)
+ *     rotated th about the z axis
+ *     given top & bottom color
+ *     given side color
+ */
+void drawCylinder(double x,double y,double z,
+                 double dx,double dy,double dz,
+                 double th, char lcol, char scol) {
+   double i;
+   double radius = 0.3, height = 0.2;
+
+   glPushMatrix();
+   //  Transform cube
+   glTranslated(x,y,z);
+   glRotated(th,0,0,1);
+   glScaled(dx,dy,dz);
+
+   //Set colors
+   if (scol == 'r'){
+      glColor3f(1.0, 0.0, 0.0); //Set color to red
+   } else if (scol == 'g') {
+      glColor3f(0.0, 1.0, 0.0); //Set color to green
+   } else if (scol == 'b') {
+      glColor3f(0.0, 0.0, 1.0); //Set color to blue
+   } else if (scol == 'w') {
+      glColor3f(1.0, 1.0, 1.0); //Set color to white
+   } else {
+      glColor3f(0.8, 0.8, 0.8); //Default color to gray
+   }
+
+
+   /* Tube of Cylinder */
+   glBegin(GL_QUAD_STRIP);
+      for(i = 0.0; i <= 2.1*PI; i+=.05){
+         glVertex3d(radius * cos(i), height, radius * sin(i));
+         glVertex3d(radius * cos(i), -height, radius * sin(i));
+      }
+   glEnd();
+
+   //Set colors
+   if (lcol == 'r'){
+      glColor3f(1.0, 0.0, 0.0); //Set color to red
+   } else if (lcol == 'g') {
+      glColor3f(0.0, 1.0, 0.0); //Set color to green
+   } else if (lcol == 'b') {
+      glColor3f(0.0, 0.0, 1.0); //Set color to blue
+   } else if (lcol == 'w') {
+      glColor3f(1.0, 1.0, 1.0); //Set color to white
+   } else {
+      glColor3f(0.8, 0.8, 0.8); //Default color to gray
+   }
+
+   /* Top of Cylinder */
+   glBegin(GL_TRIANGLE_FAN);
+      glVertex3d(0.0, height, 0.0);
+
+      for(i = 0.0; i < 2*PI*radius*4; i+=.125) {
+         glVertex3d(radius * cos(i), height, radius * sin(i));
+      }
+   glEnd();
+
+   /* Bottom of Cylinder */
+   glBegin(GL_TRIANGLE_FAN);
+      glVertex3d(0.0, -height, 0.0);
+
+      for(i = 0.0; i < 2*PI*radius*4; i+=.125) {
+         glVertex3d(radius * cos(i), -height, radius * sin(i));
+      }
+   glEnd();
+
+   glPopMatrix();
+}
+
+/*
+ *  Display the scene
+ *  Taken from HW3 and Example 9
+ */
+void display()
+{
+   const double len=1.5;  //  Length of axes
+   //  Erase the window and the depth buffer
+   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+   //  Enable Z-buffering in OpenGL
+   glEnable(GL_DEPTH_TEST);
+   //  Undo previous transformations
+   glLoadIdentity();
+
+   if (fp) {
+      Cx = +2*dim*Sin(rot); //Ajust the camera vector based on rot
+      Cz = -2*dim*Cos(rot);
+
+      gluLookAt(Ex,Ey,Ez, Cx+Ex,Ey,Cz+Ez, 0,1,0); //  Use gluLookAt, y is the up-axis
+
+   }
+   else {
+      //  Perspective - set eye position
+      if (mode)
+      {
+         double px = -2*dim*Sin(th)*Cos(ph);
+         double py = +2*dim        *Sin(ph);
+         double pz = +2*dim*Cos(th)*Cos(ph);
+         gluLookAt(px,py,pz , 0,0,0 , 0,Cos(ph),0);
+      }
+      //  Orthogonal - set world orientation
+      else
+      {
+         glRotatef(ph,1,0,0);
+         glRotatef(th,0,1,0);
+      }
+   }
+
+   /* Draw 4 Clyinders */   
+   drawCylinder(0, 0, 0, 1, 1, 1, 0, 'r', 'b');
+   drawCylinder(.5, .5, .5, 1, 3, 1, 20, 'b', 'q');
+   drawCylinder(-.9, 0, 0, 2, 2, 1, 90, 'w', 'g');
+   drawCylinder(-.6, -.6, -.8, .3, .5, .3, -45, 'g', 'r');
+
+   //  Draw axes
+   glColor3f(1,1,1);
+   if (axes)
+   {
+      glBegin(GL_LINES);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(len,0.0,0.0);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(0.0,len,0.0);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(0.0,0.0,len);
+      glEnd();
+      //  Label axes
+      glRasterPos3d(len,0.0,0.0);
+      Print("X");
+      glRasterPos3d(0.0,len,0.0);
+      Print("Y");
+      glRasterPos3d(0.0,0.0,len);
+      Print("Z");
+   }
+   //  Display parameters
+   glWindowPos2i(5,5);
+   if (fp) {
+      Print("FP=On View Angle=%d",rot);
+   }
+   else {
+      Print("FP=Off Angle=%d,%d  Dim=%.1f FOV=%d Projection=%s",th,ph,dim,fov,mode?"Perpective":"Orthogonal");
+   }
+   //  Render the scene and make it visible
+   glFlush();
+   glutSwapBuffers();
+}
+
+/*
+ *  GLUT calls this routine when an arrow key is pressed
+ */
+void special(int key,int x,int y)
+{
+   if(!fp) {
+      //  Right arrow key - increase angle by 5 degrees
+      if (key == GLUT_KEY_RIGHT)
+         th += 5;
+      //  Left arrow key - decrease angle by 5 degrees
+      else if (key == GLUT_KEY_LEFT)
+         th -= 5;
+      //  Up arrow key - increase elevation by 5 degrees
+      else if (key == GLUT_KEY_UP)
+         ph += 5;
+      //  Down arrow key - decrease elevation by 5 degrees
+      else if (key == GLUT_KEY_DOWN)
+         ph -= 5;
+      //  PageUp key - increase dim
+      else if (key == GLUT_KEY_PAGE_UP && dim>1)
+         dim -= 0.1;
+      //  PageDown key - decrease dim
+      else if (key == GLUT_KEY_PAGE_DOWN)
+         dim += 0.1;
+      //  Keep angles to +/-360 degrees
+      th %= 360;
+      ph %= 360;
+   }
+   //  Update projection
+   Project();
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when a key is pressed
+ */
+void key(unsigned char ch,int x,int y)
+{
+   //  Exit on ESC
+   if (ch == 27)
+      exit(0);
+   //  Toggle axes
+   else if (ch == '9')
+      axes = 1-axes;
+   //  Toggle first person
+   else if (ch == 'f' || ch == 'F')
+   {
+      fp = 1-fp;
+   }
+
+   if (fp) {
+      double dt = 0.05;
+      if (ch == 'w' || ch == 'W'){
+         Ex += Cx*dt; //Update the eye vector based on the camera vector
+         Ez += Cz*dt;
+      }
+      else if (ch == 'a' || ch == 'A'){
+         rot -= 3;
+      }
+      else if (ch == 's' || ch == 'S'){
+         Ex -= Cx*dt;
+         Ez -= Cz*dt;
+      }
+      else if (ch == 'd' || ch == 'D'){
+         rot += 3;
+      }
+
+      //  Keep angles to +/-360 degrees
+      rot %= 360;
+   }
+   else {
+      //  Reset view angle
+      if (ch == '0')
+         th = ph = 0;
+      //  Switch display mode
+      else if (ch == 'm' || ch == 'M')
+         mode = 1-mode;
+      //  Change field of view angle
+      else if (ch == '1')
+         fov--;
+      else if (ch == '2')
+         fov++;
+   }
+   
+   //  Reproject
+   Project();
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when the window is resized
+ */
+void reshape(int width,int height)
+{
+   //  Ratio of the width to the height of the window
+   asp = (height>0) ? (double)width/height : 1;
+   //  Set the viewport to the entire window
+   glViewport(0,0, width,height);
+   //  Set projection
+   Project();
+}
+
+/*
+ *  Start up GLUT and tell it what to do
+ */
+int main(int argc,char* argv[])
+{
+   //  Initialize GLUT
+   glutInit(&argc,argv);
+   //  Request double buffered, true color window with Z buffering at 600x600
+   glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+   glutInitWindowSize(600,600);
+   glutCreateWindow("Taylor Andrews");
+   //  Set callbacks
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutSpecialFunc(special);
+   glutKeyboardFunc(key);
+   //  Pass control to GLUT so it can interact with the user
+   glutMainLoop();
+   return 0;
+}
